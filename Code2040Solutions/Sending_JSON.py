0f14a2967ae42f4ca96c70db4ae3182fdd1736a0
Samuel Triana <sxt322@miami.edu>
2017-12-02 15:23:37 -0500

    Delete Sending_JSON.py

--- Code2040Solutions/Sending_JSON.py
+++
@@ -1,5 +0,0 @@
-// Python Script used to complete Challenge 1, making use of the Requests library
-
-import requests
-
-requests.post('http://challenge.code2040.org/api/register', json = {'token':'16f6075adf4d9f4d209314475bc27e4b','github':'https://github.com/fang0119/Code-2040'})
